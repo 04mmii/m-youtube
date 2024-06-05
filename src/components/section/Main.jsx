682df77dbcefda69464cee7fa6dc887e03ec509e
04mmii <you@example.com>
2024-06-05 11:49:08 +0900

    페이지 SEO작업

--- src/components/section/Main.jsx
+++ src/components/section/Main.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import Headder from './Header';
 import Footer from './Footer';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 const Main = (props) => {
   return (
-    <>
+    <HelmetProvider>
+      <Helmet
+        titleTemplate="%s | Webs Youtube" 
+        defaultTitle="Webs Youtube" 
+        defer={false}
+      >
+          {props.title && <title>{props.title}</title>}
+          <meta name="description" content={props.description} />
+      </Helmet>
       <Headder />
       <main id='main' role='main'>
         {props.children}
       </main>
       <Footer />
-    </>
+    </HelmetProvider>
+
+
   )
 }
 
